50366fcc0daa32ce20f033b76a2ae53ba9b66d1f
chawanakorns <chawanakorn_s@cmu.ac.th>
2024-09-10 19:06:42 +0700

    3.3

--- src/main/java/se331/lab/rest/controller/EventController.java
+++ src/main/java/se331/lab/rest/controller/EventController.java
@@ -1,9 +1,13 @@
 package se331.lab.rest.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import se331.lab.entity.Event;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
+import se331.lab.rest.entity.Event;
 
 import jakarta.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -86,7 +90,40 @@ public class EventController {
     }
 
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit",
+            required = false)Integer perPage
+    ,@RequestParam(value = "_page", required = false)Integer page) {
+        perPage = perPage == null ? eventList.size() : perPage;
+        page = page == null ? 1 : page;
+        Integer firstIndex = (page - 1) * perPage;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + perPage; i++) {
+            output.add(eventList.get(i));
+        }
+        try {
+            for (int i = firstIndex; i < firstIndex + perPage; i++) {
+                output.add(eventList.get(i));
+            }
+            return ResponseEntity.ok(output);
+        } catch (IndexOutOfBoundsException ex){
+            return ResponseEntity.ok(output);
+        }
+    }
+
+    @GetMapping("events/{id}")
+    public ResponseEntity<?> getEvent(@PathVariable("id") Long id){
+        Event output = null;
+        for (Event event :
+        eventList) {
+            if (event.getId().equals(id)) {
+                output = event;
+                break;
+            }
+        }
+        if (output != null){
+            return ResponseEntity.ok(output);
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "The given id is not found");
+        }
     }
 }
