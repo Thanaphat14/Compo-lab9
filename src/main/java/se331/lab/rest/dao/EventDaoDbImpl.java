d89f0c8d8aff91f0e95de06e89202490c260f1ec
chawanakorns <chawanakorn_s@cmu.ac.th>
2024-09-16 16:55:15 +0700

    Lab 7 (4.7)

--- src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
+++ src/main/java/se331/lab/rest/dao/EventDaoDbImpl.java
@@ -2,16 +2,16 @@ package se331.lab.rest.dao;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Repository;
 import se331.lab.rest.entity.Event;
 import se331.lab.rest.repository.EventRepository;
 
-import java.util.List;
-
 @Repository
 @RequiredArgsConstructor
 @Profile("db")
-public class EventDaoDblmpl implements EventDao {
+public class EventDaoDbImpl implements EventDao {
     final EventRepository eventRepository;
     @Override
     public Integer getEventSize() {
@@ -19,13 +19,8 @@ public class EventDaoDblmpl implements EventDao {
     }
 
     @Override
-    public List<Event> getEvents(Integer pageSize, Integer page) {
-        List<Event> events = eventRepository.findAll();
-        pageSize = pageSize == null ? events.size() : pageSize;
-        page = page == null ? 1 : page;
-        int firstIndex = (page - 1) * pageSize;
-        List<Event> output = events.subList(firstIndex, firstIndex + pageSize);
-        return output;
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
+        return eventRepository.findAll(PageRequest.of(page - 1, pageSize));
     }
 
     @Override
